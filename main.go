cdd9c6d8ba00ff628ec6cdc27076a1ae9dec6207
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-01-29 13:11:26 +0300

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -9,6 +10,9 @@ import (
 
 func main() {
 	g := game.NewGame()
+	fps := ebiten.ActualFPS()
+	title := fmt.Sprint("Tempest FPS: %v", fps)
+	ebiten.SetWindowTitle(title)
 	ebiten.SetWindowSize(1470, 956)
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
